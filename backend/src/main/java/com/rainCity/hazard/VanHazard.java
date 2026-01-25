4aa19cdd96ff930d9cfadb456a06c833a73af5c3
Harman Bhogal <bhogalharman7@gmail.com>
2026-01-24 23:19:43 -0800

    changed main file by adding scheduling

--- backend/src/main/java/com/rainCity/hazard/VanHazard.java
+++ backend/src/main/java/com/rainCity/hazard/VanHazard.java
@@ -1,9 +1,12 @@
 package com.rainCity.hazard;
 
+// imports
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VanHazard {
   public static void main(String[] args) {
     SpringApplication.run(VanHazard.class, args);
